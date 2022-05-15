cadba1b7820e295f6651001da7a3db74151976d5
Matias Lazzaroni <matias_lazzaroni@hotmail.com>
2022-05-15 01:15:21 -0300

    agregando navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import './NavBar.css'
+
+const NavBar = () => {
+
+    return (
+
+        <nav>
+            <div><img src='./images/logoToctocNavBar.svg' alt='logoTocNavBar'></img></div>
+            
+            <ul>
+                <li>Inicio</li>
+                <li>Nosotros</li>
+                <li>Productos</li>
+            </ul>
+        </nav>
+
+
+    )
+}
+
+export default NavBar
