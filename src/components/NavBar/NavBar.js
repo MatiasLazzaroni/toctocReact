efaa39a49fec34a401ef12a5328afbaf356822cf
Matias Lazzaroni <matias_lazzaroni@hotmail.com>
2022-05-15 17:38:00 -0300

    menu NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,17 +3,18 @@ import './NavBar.css'
 const NavBar = () => {
 
     return (
-
-        <nav>
-            <div><img src='./images/logoToctocNavBar.svg' alt='logoTocNavBar'></img></div>
+        <div className='menu'>
+            <div className='logoToc'><img  src='./images/logoToctocNavBar.svg' alt='logoTocNavBar'></img></div>
             
-            <ul>
-                <li>Inicio</li>
-                <li>Nosotros</li>
-                <li>Productos</li>
-            </ul>
-        </nav>
-
+            <nav>      
+                <ul>
+                
+                    <li>Inicio</li>
+                    <li>Nosotros</li>
+                    <li>Productos</li>
+                </ul>
+            </nav>
+        </div>
 
     )
 }
